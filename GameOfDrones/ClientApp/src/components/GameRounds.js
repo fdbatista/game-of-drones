40c670504824a58843c53c74318249a6c7bcc7c3
FD <fdbatista@gmail.com>
2019-03-09 23:05:29 -0500

    Finished battle

--- GameOfDrones/ClientApp/src/components/GameRounds.js
+++ GameOfDrones/ClientApp/src/components/GameRounds.js
@@ -20,27 +20,23 @@ export class GameRounds extends Component {
         this.handleChange = this.handleChange.bind(this);
         this.setWeapon = this.setWeapon.bind(this);
         this.difference = this.difference.bind(this);
-    }
-
-    componentDidMount() {
-        let rounds = Object.assign([], this.state.rounds);
-        rounds.push(this.state.currentRound);
-        this.setState({ rounds });
+        this.renderFinishedRounds = this.renderFinishedRounds.bind(this);
+        this.getGameWinnerIndex = this.getGameWinnerIndex.bind(this);
     }
 
     setWeapon() {
-        if (this.state.currentPlayerIndex < Object.keys(this.props.players).length - 1) {
-            this.setState({ currentPlayerIndex: this.state.currentPlayerIndex + 1 });
+        if (this.state.currentPlayerIndex < Object.keys(this.props.players).length - 1) {   //This is player 1 choosing a weapon
+            this.setState({ currentPlayerIndex: this.state.currentPlayerIndex + 1 });   //We set the index for player 2
         } else {
             let currentRound = Object.assign({}, this.state.currentRound);
-            let winnerIndex, difference = this.difference(currentRound.weapons[0], currentRound.weapons[1]);
+            let winnerIndex, difference = this.difference(currentRound.weapons[0], currentRound.weapons[1]);    //We calculate the difference between the chosen weapons' indices
             if (difference === 1) {
-                winnerIndex = currentRound.weapons[0] > currentRound.weapons[1] ? 0 : 1;
+                winnerIndex = currentRound.weapons[0] > currentRound.weapons[1] ? 0 : 1;    //If they picked consecutive weapons, the lowest index wins the round
             }
             else if (difference > 1) {
-                winnerIndex = currentRound.weapons[0] < currentRound.weapons[1] ? 0 : 1;
+                winnerIndex = currentRound.weapons[0] < currentRound.weapons[1] ? 0 : 1;    //If the difference is greater than 1, the highest index wins
             } else {
-                winnerIndex = -1
+                winnerIndex = -1    //Otherwise, the round ends with a tie because they selected the same weapon
             }
             currentRound.weapons[0] = this.state.weapons[currentRound.weapons[0]];    //Setting weapon name instead of its index
             currentRound.weapons[1] = this.state.weapons[currentRound.weapons[1]];    //Setting weapon name instead of its index
@@ -50,14 +46,35 @@ export class GameRounds extends Component {
             this.setState({ currentRound });
 
             let rounds = Object.assign([], this.state.rounds);
-            rounds.splice(rounds.length - 1, 1, currentRound);
-            currentRound = { weapons: ["", ""], winner: "", finished: 0 };
             rounds.push(currentRound);
+            currentRound = { weapons: ["", ""], winner: "", finished: 0 };
             this.setState({ currentRound });
             this.setState({ currentPlayerIndex: 0 });
             this.setState({ rounds });
-            //this.props.gameFinishedCallback(currentRound);
+
+            let gameWinnerIndex = this.getGameWinnerIndex(rounds);
+            if (gameWinnerIndex >= 0) {
+                this.props.finishedGameCallback({ gameWinnerIndex: gameWinnerIndex, rounds: rounds.length });
+            }
         }
+    }
+
+    getGameWinnerIndex(rounds) {
+        let res = [0, 0];
+        let i;
+        for (i in rounds) {
+            let round = rounds[i];
+            if (round.winnerIndex !== -1) {
+                res[round.winnerIndex]++;
+                if (res[round.winnerIndex] > 2) {
+                    return round.winnerIndex;
+                }
+            }
+        }
+        console.clear();
+        console.log(rounds);
+        console.log(res);
+        return -1;
     }
 
     difference(a, b) {
@@ -70,15 +87,38 @@ export class GameRounds extends Component {
         this.setState({ round });
     }
 
+    renderFinishedRounds() {
+        return (
+            <table className='table table-striped'>
+                <thead>
+                    <tr>
+                        <th>Round</th>
+                        <th>Winner</th>
+                        <th>Weapons</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {this.state.rounds.map((round, index) =>
+                        <tr key={index}>
+                            <td>{index + 1}</td>
+                            <td><span className={round.winnerIndex !== -1 ? "text-primary" : "text-warning"}>{round.winner}</span></td>
+                            <td>{round.weapons[0] + ' vs ' + round.weapons[1]}</td>
+                        </tr>
+                    )}
+                </tbody>
+            </table>
+        );
+    }
+
     render() {
         let weaponForm;
         if (this.state.gameFinished === 0) {
             weaponForm =
                 <div className="row">
-                <div className="col-sm-12 col-lg-5 bordered mt-2">
+                    <div className="col-sm-12 col-lg-5 bordered mt-2">
                         <Animated animationIn="fadeIn">
                             <div>
-                                <span className="badge default text-light text-uppercase">Round {this.state.rounds.length}</span>
+                                <span className="badge default text-light text-uppercase">Round {this.state.rounds.length + 1}</span>
                                 <h5>{this.props.players[this.state.currentPlayerIndex].name}</h5>
                                 <hr />
                                 <select className="form-control mb-2" value={this.state.currentRound.weapons[this.state.currentPlayerIndex]} onChange={this.handleChange}>
@@ -96,15 +136,7 @@ export class GameRounds extends Component {
                         </Animated>
                     </div>
                     <div className="col-sm-12 mt-2 col-lg-5 offset-lg-2 bordered mt-2">
-                        {
-                            [1, 2, 3, 4].map((item, index) => {
-                                return (
-                                    <div key={index}>
-                                        <h3 className="text-center">{item}</h3>
-                                    </div>
-                                )
-                            })
-                        }
+                        {this.renderFinishedRounds()}
                     </div>
                 </div>
         }
--- GameOfDrones/ClientApp/src/components/NewGame.js
+++ GameOfDrones/ClientApp/src/components/NewGame.js
@@ -11,8 +11,8 @@ export class NewGame extends Component {
         
         this.state = {
             players: [
-                { name: "", battleWinner: 0 },
-                { name: "", battleWinner: 0 }
+                { name: "", gameWinner: 0, roundsWon: 0 },
+                { name: "", gameWinner: 0, roundsWon: 0 }
             ],
             winner: "",
             gameStatus: 0
@@ -21,12 +21,12 @@ export class NewGame extends Component {
         this.handleInputChange = this.handleInputChange.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
         this.beginGame = this.beginGame.bind(this);
-        this.addRound = this.addRound.bind(this);
-        this.getRoundResults = this.getRoundResults.bind(this);
+        this.restartGame = this.restartGame.bind(this);
+        this.getGameResults = this.getGameResults.bind(this);
     }
 
     handleInputChange(event) {
-        let players = Object.assign({}, this.state.players);
+        let players = Object.assign([], this.state.players);
         var target = event.target;
         var index = target.name;
         players[index].name = target.value;
@@ -34,57 +34,61 @@ export class NewGame extends Component {
     }
 
     beginGame() {
-        this.addRound();
         this.setState({ gameStatus: 1 });
     }
 
-    addRound() {
-        let rounds = Object.assign([], this.state.rounds);
-        rounds.push({});
-        this.setState({ rounds });
+    restartGame() {
+        this.setState({players: [{ name: "", gameWinner: 0, roundsWon: 0 }, { name: "", gameWinner: 0, roundsWon: 0 }] });
+        this.setState({ gameStatus: 0 });
     }
 
     handleSubmit(event) {
         event.preventDefault();
     }
 
-    getRoundResults(finishedRound) {
-        if (finishedRound.finished === 1) {
-            let rounds = Object.assign([], this.state.rounds);
-            rounds.splice(rounds.length - 1, 1, finishedRound);
-            if (finishedRound.winnerIndex > -1) {
-                let players = Object.assign([], this.state.players);
-                if (++players[finishedRound.winnerIndex].wonRounds === 3) {
-                    players[finishedRound.winnerIndex].battleWinner = 1;
-                } else {
-                    rounds.push({ finished: 0 });
-                }
-                this.setState({ players });
-                console.log(players);
-            }
-            this.setState({ rounds });
-            console.clear();
-            console.log(rounds);
-        }
+    getGameResults(results) {
+        console.clear();
+        console.log(results);
+        let players = Object.assign([], this.state.players);
+        players[results.gameWinnerIndex].gameWinner = 1;
+        players[results.gameWinnerIndex].roundsWon = results.rounds;
+        this.setState({ players });
+        this.setState({ gameStatus: 2 });
     };
 
     render() {
-        let gameRounds, playersNamesForm;
-        if (this.state.gameStatus === 1) {
-            gameRounds = <GameRounds players={this.state.players} gameFinishedCallback={this.getRoundResults} />
-        } else {
-            playersNamesForm = 
-                <div className="row">
-                    <div className="col-sm-12 col-md-6 col-lg-4">
-                        <form onSubmit={this.handleSubmit}>
-                            <input maxlength="25" name="0" type="text" className="form-control mb-2" placeholder="First warrior's name" value={this.state.players[0].name} onChange={this.handleInputChange} />
-                            <input maxlength="25" name="1" type="text" className="form-control mb-2" placeholder="Second warrior's name" value={this.state.players[1].name} onChange={this.handleInputChange} />
-                        </form>
-                    <button disabled={!this.state.players[0].name || !this.state.players[1].name} className="btn btn-primary form-control" onClick={this.beginGame}><GiCrestedHelmet size="1.3em" /> Valar Morghulis</button>
-                    </div>
-                </div>
-        }
+        let visibleForm;
 
+        switch (this.state.gameStatus) {
+            case 0:
+                visibleForm =
+                    <div className="row">
+                        <div className="col-sm-12 col-md-6 col-lg-4">
+                            <form onSubmit={this.handleSubmit}>
+                                <input maxLength="25" name="0" type="text" className="form-control mb-2" placeholder="First warrior's name" value={this.state.players[0].name} onChange={this.handleInputChange} />
+                                <input maxLength="25" name="1" type="text" className="form-control mb-2" placeholder="Second warrior's name" value={this.state.players[1].name} onChange={this.handleInputChange} />
+                            </form>
+                            <button disabled={!this.state.players[0].name || !this.state.players[1].name} className="btn btn-primary form-control" onClick={this.beginGame}><GiCrestedHelmet size="1.3em" /> Valar Morghulis</button>
+                        </div>
+                    </div>
+                break;
+            case 1:
+                visibleForm = <GameRounds players={this.state.players} finishedGameCallback={this.getGameResults} />
+                break;
+            case 2:
+                visibleForm = 
+                    <div className="row">
+                        <div className="col-sm-12 text-center">
+                            <h5>We have a WINNER!</h5>
+                            <p>After {this.state.players[0].gameWinner === 1 ? this.state.players[0].roundsWon : this.state.players[1].roundsWon} rounds, <b>{this.state.players[0].gameWinner === 1 ? this.state.players[0].name : this.state.players[1].name}</b> has become the First of his/her Name!</p>
+                            <button className="btn btn-primary" onClick={this.restartGame}><GiCrestedHelmet size="1.3em" /> Try again</button>
+                        </div>
+                    </div>
+                break;
+            default:
+                break;
+        }
+        
         return (
             <div className="container">
                 <div className="row">
@@ -99,8 +103,7 @@ export class NewGame extends Component {
                     </div>
                 </div>
 
-                {playersNamesForm}
-                {gameRounds}
+                {visibleForm}
             </div>
         );
     }
